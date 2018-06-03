d5187337188a02da6edcdbdd0466ad7959eff285
Sarah Grimm <sarahagrimm@gmail.com>
2018-06-03 02:03:00 -0700

    understanding interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,203 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+// Classes
+class Greeting {
+  message: string;
+
+  constructor(message: string) {
+    this.message = message;
+  }
+
+  greet() {
+    return "Hello, " + this.message;
+  }
+}
+
+class Animal {
+  private name: string;
+
+  constructor(animalName: string) {
+    this.name = animalName;
+  }
+
+  move(distance: number = 0) {
+    console.log(`${this.name} moved ${distance}m.`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name: string) {
+    super(name);
+  }
+
+  bark() {
+    console.log('Woof!');
+  }
+
+  move(distanceMovedByDog: number = 5) {
+    console.log('dog is moving...');
+    super.move(distanceMovedByDog);
+  }
+}
+
+class Poodle extends Dog {
+  constructor(name: string) {
+    super(name);
+  }
+
+  move() {
+    let moving = super.move();
+    console.log('poodle is walking...');
+  }
+}
+
+class Horse extends Animal {
+  // create constructor to set Animal name
+  constructor(name: string) {
+    super(name);
+  }
+
+  // create move to set default distance to 45
+  move(distanceMovedByHorse: number = 45) {
+    console.log('Horse is charging...');
+    super.move(distanceMovedByHorse);
+  }
+}
+
+class User {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class John extends User {
+  constructor() { 
+    super("John");
+  }
+}
+
+class Employee {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class Person {
+  protected name: string;
+
+  protected constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class Worker extends Person {
+  private department: string;
+
+  constructor(name: string, department: string) {
+    super(name);
+    this.department = department;
+  }
+
+  getDetails() {
+    return `My name is ${this.name} and I work in ${this.department}.`;
+  }
+}
+// End Classes
+// Interfaces
+interface Stuff {
+  name: string;
+  age: number;
+  department?: string;
+
+  foo?(s: string);
+  // foo?(n: number);
+}
+
+class StuffTwo implements Stuff {
+  name: string;
+  age: number;
+
+  foo?(s: string) {
+    this.name = s;
+  }
+
+}
+// End Interface
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'app works!';
+
+  ngOnInit() {
+    // this.classTesting();
+    // this.animalClassTesting();
+    // this.classCompatibilityTesting();
+    // this.extendDerivedClassTesting();
+    // this.protectedTesting();
+    this.interfaceTesting({ name: "xyz", age: 25 });
+  }
+
+  // Class testing
+  classTesting() {
+    const greeter = new Greeting('world!');
+    console.log(greeter.greet());
+  }
+
+  animalClassTesting() {
+    const dog = new Dog("Goofy");
+    dog.bark();
+    dog.move();
+
+    const horse: Animal = new Horse("Bulls eye");
+    horse.move(20);
+
+    const animal = new Animal("Cat");
+    console.log(animal);
+  }
+
+  classCompatibilityTesting() {
+    let user = new User("Doe");
+    const john = new John();
+
+    console.log(user);
+    console.log(john);
+
+    const employee = new Employee("Smith");
+    console.log(employee);
+
+    user = john; // compatible because User and John use same private field (name)
+    console.log(user);
+
+    // user = employee; // incompatible since User and Employee have different private fields (name)
+  }
+
+  extendDerivedClassTesting() {
+    const poodle = new Poodle("Tom");
+    poodle.move();
+  }
+
+  protectedTesting() {
+    const worker = new Worker("Steve", "Sales");
+    console.log(worker.getDetails());
+    // console.log(worker.name);
+    // const person = new Person("Patrick");
+    // console.log(person);
+  }
+  // End Class testing
+  // Interface testing
+  interfaceTesting(x: Stuff) {
+    console.log(`${x.name} is ${x.age} years old and works in ${x.department}.`);
+    // x.foo(5);
+    const stuff = new StuffTwo();
+    stuff.foo("test");
+    console.log(stuff.name);
+  }
+  // End Interface testing
 }
